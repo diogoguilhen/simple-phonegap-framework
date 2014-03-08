36da34d6c1ee8485c6345350dcaf1ef3d173fb0b
Bastian Meier <post@bastianmeier.de>
2014-03-08 01:07:48 +0100

    adding camera demo

--- www/js/pages/imageDemo.js
+++ www/js/pages/imageDemo.js
@@ -12,11 +12,51 @@ app.pages.imageDemo = {
         $('#camera-button').unbind('click')
             .click(function () {
                 console.log('trying to take picture');
-                if (navigator.camera !== undefined){
-                    $('#camera-error').html('navigator.camera is definded').fadeIn('slow');
+                var text = '';
+                if (navigator.camera !== undefined) {
+                    text += 'navigator.camera is definded';
                 } else {
-                    $('#camera-error').html('navigator.camera is undefinded').fadeIn('slow');
+                    text += 'navigator.camera is undefinded';
                 }
+                if (navigator.camera.getPicture !== undefined) {
+                    text += '<br>navigator.camera.getPicture is definded';
+                } else {
+                    text += '<br>navigator.camera.getPicture is undefinded';
+                }
+                if (typeof navigator.camera.getPicture !== 'function') {
+                    text += '<br>navigator.camera.getPicture is not a function';
+                } else {
+                    text += '<br>navigator.camera.getPicture is a function';
+                }
+                if (navigator.camera.DestinationType !== undefined) {
+                    text += '<br>navigator.camera.DestinationType is definded';
+                } else {
+                    text += '<br>navigator.camera.DestinationType is undefinded';
+                }
+                if (navigator.camera.DestinationType.DATA_URL !== undefined) {
+                    text += '<br>navigator.camera.DestinationType.DATA_URL is definded';
+                    text += '<br>navigator.camera.DestinationType.DATA_URL: ' + navigator.camera.DestinationType.DATA_URL;
+                } else {
+                    text += '<br>navigator.camera.DestinationType.DATA_URL is undefinded';
+                }
+                if (Camera !== undefined) {
+                    text += '<br>Camera is definded';
+                } else {
+                    text += '<br>Camera is undefinded';
+                }
+                if (Camera.DestinationType !== undefined) {
+                    text += '<br>Camera.DestinationType is definded';
+                } else {
+                    text += '<br>Camera.DestinationType is undefinded';
+                }
+                if (Camera.DestinationType.DATA_URL !== undefined) {
+                    text += '<br>Camera.DestinationType.DATA_URL is definded';
+                    text += '<br>Camera.DestinationType.DATA_URL : ' + Camera.DestinationType.DATA_URL;
+                } else {
+                    text += '<br>Camera.DestinationType.DATA_URL is undefinded';
+                }
+
+                $('#camera-error').html(text).fadeIn('slow');
                 navigator.camera.getPicture(this.onSuccess, this.onFail, { quality: 20,
                     destinationType: navigator.camera.DestinationType.DATA_URL
                 });
